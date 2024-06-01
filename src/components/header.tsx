a07993c7e9e242f5cd90e21f3ed6e41c1eb10945
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-01 19:13:32 -0300

    Add theme and toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { Constants } from '@/constants'
 
 import NavLink from './navLink'
+import { ThemeToggle } from './theme/themeToggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -23,6 +24,9 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
