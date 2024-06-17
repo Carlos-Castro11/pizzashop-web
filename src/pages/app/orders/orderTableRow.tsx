1d8512d60f36ff2d5a7815501e9010f561557018
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-17 20:32:58 -0300

    Add cancel order functionality

--- src/pages/app/orders/orderTableRow.tsx
+++ src/pages/app/orders/orderTableRow.tsx
@@ -9,6 +9,9 @@ import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './orderDetails'
 import { OrderStatus } from './orderStatus'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { GetCancelOrderFn } from '@/api/cancelOrder'
+import { GetOrdersResponse } from '@/api/getOrders'
 
 interface OrderTableRowProps {
   order: {
@@ -22,6 +25,33 @@ interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  const { mutateAsync: cancelOrder } = useMutation({
+    mutationFn: GetCancelOrderFn,
+    async onSuccess(_, { orderId }) {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ['orders']
+      })
+
+      ordersListCache.forEach(([cacheKey, cacheData]) => {
+        if (!cacheData) {
+          return
+        }
+
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map(order => {
+            if (order.orderId === orderId) {
+              return { ...order, status: 'canceled' }
+            }
+
+            return order
+          })
+        })
+      })
+    },
+  })
 
   return (
     <TableRow>
@@ -64,7 +94,10 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         </Button>
       </TableCell>
       <TableCell>
-        <Button variant="ghost" size="xs">
+        <Button
+          onClick={() => cancelOrder({ orderId: order.orderId })}
+          disabled={!['pending', 'processing'].includes(order.status)}
+          variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
