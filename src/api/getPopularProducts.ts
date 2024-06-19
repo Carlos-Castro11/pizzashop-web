9df084023af217f8cf59e9080ab05f633e51abb2
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-19 19:53:13 -0300

    Add metrics skeleton

--- src/api/getPopularProducts.ts
+++ src/api/getPopularProducts.ts
@@ -3,7 +3,7 @@ import { api } from '@/lib/axios'
 export type GetPopularProductsResponse = {
   product: string
   amount: number
-}
+}[]
 
 export async function GetPopularProducts() {
   const response = await api.get<GetPopularProductsResponse>(
